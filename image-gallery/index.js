9d71e8119ae0db839d233cf68e9ac65af830e50d
@Drikkii <chevpavel@gmail.com>
2023-09-26 17:53:59 +0300

    feat: js structure and logic

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,75 +1,68 @@
-import ReactDOM from "react-dom";
-import React, { Fragment, useEffect, useState } from "react";
-import "./style.css";
-import { createApi } from "unsplash-js";
+const CLIENT_ID = "2pWl7-TpYiOG_JLrRElAF-1zHW-9UlPuXJh0XgDeHek";
+const slider = document.getElementById("img-api");
+const input = document.querySelector(".input");
+const search = document.querySelector(".search");
+const cross = document.querySelector(".cross");
 
-const api = createApi({
-  // Don't forget to set your access token here!
-  // See https://unsplash.com/developers
-  accessKey: "",
+// Api запрос обработка
+
+let url = `https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}&count=27&query=mountains`;
+
+async function randomPhoto() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  slider.innerHTML = data
+    .map(({ urls: { small, raw } }) => {
+      return `<a href="${raw}"><div class="class-img"  style="background-image: url(${small})"></div></a>
+             `;
+    })
+    .join("");
+}
+
+search.addEventListener("click", function () {
+  url = `https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}&count=9&query=${input.value}`;
+  randomPhoto();
 });
 
-const PhotoComp = ({ photo }) => {
-  const { user, urls } = photo;
+randomPhoto();
 
-  return (
-    <Fragment>
-      <img className="img" src={urls.regular} />
-      <a
-        className="credit"
-        target="_blank"
-        href={`https://unsplash.com/@${user.username}`}
-      >
-        {user.name}
-      </a>
-    </Fragment>
-  );
-};
+// Поиск по Enter
 
-const Body = () => {
-  const [data, setPhotosResponse] = useState(null);
-
-  useEffect(() => {
-    api.search
-      .getPhotos({ query: "cat", orientation: "landscape" })
-      .then((result) => {
-        setPhotosResponse(result);
-      })
-      .catch(() => {
-        console.log("something went wrong!");
-      });
-  }, []);
-
-  if (data === null) {
-    return <div>Loading...</div>;
-  } else if (data.errors) {
-    return (
-      <div>
-        <div>{data.errors[0]}</div>
-        <div>{document.querySelector(".img-api")}</div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="feed">
-        <ul className="columnUl">
-          {data.response.results.map((photo) => (
-            <li key={photo.id} className="li">
-              <PhotoComp photo={photo} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+input.addEventListener("keypress", function (e) {
+  if (e.which === 13) {
+    url = `https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}&count=9&query=${input.value}`;
+    randomPhoto();
   }
-};
+});
 
-const Home = () => {
-  return (
-    <main className="root">
-      <Body />
-    </main>
-  );
-};
+// крестик на поиске
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+document.addEventListener("mouseup", function (e) {
+  let container = document.querySelector("input");
+  if (!container.contains(e.target) && input.value == "") {
+    cross.classList.add("hidden");
+  }
+});
+
+function crossUnhide() {
+  if (input.value == "") {
+    cross.classList.remove("hidden");
+  }
+}
+function crossHide() {
+  if (input.value !== "") {
+    cross.classList.add("hidden");
+    input.value = "";
+  } else {
+    cross.classList.remove("hidden");
+  }
+}
+function crossHideside() {
+  if (input.value == "") {
+    cross.classList.add("hidden");
+  }
+}
+
+input.addEventListener("click", crossUnhide);
+cross.addEventListener("click", crossHide);
